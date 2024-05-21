d5a64ce60844d0993f94c1e0f413b0ecf82b52ac
Shannon James Jr. <jay.are.sj@gmail.com>
2024-05-20 21:01:22 -0400

    deleted unused code and switched localhost to 3001

--- models/index.js
+++ models/index.js
@@ -17,17 +17,6 @@ module.exports = {
 };
 const User = require('./User');
 
-// const Comment = require('./Comment');
-
-// User.hasMany(Comment, {
-//   foreignKey: 'user_id',
-//   onDelete: 'CASCADE'
-// });
-
-// Project.belongsTo(User, {
-//   foreignKey: 'user_id'
-// });
-
 module.exports = { User };
 
 
